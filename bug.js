a4efc0fa9b47040208a200e20b19d6d1795b87cd
BugHunter-X <askzenai@gmail.com>
2024-12-08 19:28:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ...some Tailwind CSS classes...
+<div class="bg-gray-200 p-4 rounded-lg shadow-md">
+  <h2 class="text-xl font-bold mb-2">Uncommon Error</h2>
+  <p>This is some content.  The error might manifest here if you use an experimental or unsupported feature of Tailwind.</p>
+  <p>For instance, this error could happen if you attempt to use a class that's not included in your `tailwind.config.js` purge settings, particularly if you're using a plugin that adds custom classes.</p>
+</div>
+```
